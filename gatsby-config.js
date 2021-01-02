c5a5ccf2d0d5e8df39043d518fc1a1f69f5e8c4c
Jonathan Kohlmeier <jonkohlmeier@gmail.com>
2021-01-02 12:35:19 -0600

    Initial content and style changes.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Pipe Comfort`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Jonathan Kohlmeier`,
+      summary: `a technologist living in Cedar Rapids, IA, often found with a pipe between his teeth.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
+    description: `Thoughts and dreams from behind a tobacco pipe.`,
+    siteUrl: `https://jonkohlmeier.com`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `jonkohlmeier`,
     },
   },
   plugins: [
