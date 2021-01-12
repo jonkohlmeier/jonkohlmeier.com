4e5e29a264249df736bdea67262c82fdb7267539
Jonathan Kohlmeier <jonkohlmeier@gmail.com>
2021-01-12 16:14:10 -0600

    Change/Improve Layout header and footer.

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,28 +6,22 @@ const Layout = ({ location, title, children }) => {
   const isRootPath = location.pathname === rootPath
   let header
 
-  if (isRootPath) {
+
     header = (
       <h1 className="main-heading">
-        <Link to="/">{title}</Link>
+        <Link to="/">{title}</Link> <span className="subtitle">from the pipe of Jon Kohlmeier</span>
       </h1>
+  
     )
-  } else {
-    header = (
-      <Link className="header-link-home" to="/">
-        {title}
-      </Link>
-    )
-  }
+  
 
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
       <header className="global-header">{header}</header>
       <main>{children}</main>
       <footer>
-        © {new Date().getFullYear()}, Built with
+        © {new Date().getFullYear()}, Jon Kohlmeier
         {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
       </footer>
     </div>
   )
